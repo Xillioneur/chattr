77bd14306bc92546ea259985344d97c5e6b193b4
SlickJohnson <liwa.johnson@gmail.com>
2019-02-11 12:11:16 -0800

    Implement messaging

--- public/index.js
+++ public/index.js
@@ -1,16 +1,44 @@
 $(document).ready(() => {
   const socket = io.connect();
 
+  let currentUser;
+
   $("#createUserBtn").click(e => {
     e.preventDefault();
-    let username = $("#usernameInput").val();
-    if (username.length > 0) {
-      socket.emit("new user", username);
+    if ($("#usernameInput").val().length > 0) {
+      socket.emit("new user", $("#usernameInput").val());
+      currentUser = $("#usernameInput").val();
       $(".usernameForm").remove();
+      $(".mainContainer").css("display", "flex");
+    }
+  });
+
+  $("#sendChatBtn").click(e => {
+    e.preventDefault();
+    let message = $("#chatInput").val();
+    // Make sure it's not empty
+    if (message.length > 0) {
+      // Emit the message with the current user to the server
+      socket.emit("new message", {
+        sender: currentUser,
+        message: message
+      });
+      $("#chatInput").val("");
     }
   });
 
   socket.on("new user", username => {
-    console.log(`✋ ${username} has joined the chat! ✋`);
+    console.log(`${username} has joined the chat`);
+    // Add the new user to the online users div
+    $(".usersOnline").append(`<div class="userOnline">${username}</div>`);
+  });
+
+  socket.on("new message", data => {
+    $(".messageContainer").append(`
+      <div class="message">
+        <p class="messageUser">${data.sender}: </p>
+        <p class="messageText">${data.message}</p>
+      </div>
+    `);
   });
 });
--- sockets/chat.js
+++ sockets/chat.js
@@ -2,7 +2,11 @@ module.exports = (io, socket) => {
   console.log("time to chat");
   socket.on("new user", username => {
     console.log(`✋ ${username} has joined the chat! ✋`);
-    //Send the username to all clients currently connected
     io.emit("new user", username);
+  });
+
+  socket.on("new message", data => {
+    console.log(`🎤 ${data.sender}: ${data.message} 🎤`);
+    io.emit("new message", data);
   });
 };
